bec42c0f0dd3402547373afad5941d6e16c458bc
Gaurav Raj <gauravraj954647@gmail.com>
2026-08-07 15:54:43 +0530

    Time: 3 ms (64.85%) | Memory: 8.2 MB (17.08%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
-
+    
+    
     int solveUsingMem(int n, vector<int> &dp){
-        // 1. Base Case
+    //     // 1. Base Case
+    //     if(n == 0 || n == 1) return n;
+
+    //     // 2. Check ans if already exist or not
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
+    //     // 3. dp array me ans store krke return krdo
+    //     dp[n] = solveUsingMem(n-1, dp) + solveUsingMem(n-2, dp);
+    //     return dp[n];
+
         if(n == 0 || n == 1) return n;
 
-        // 2. Check ans if already exist or not
-        if(dp[n] != -1){
-            return dp[n];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        // 3. dp array me ans store krke return krdo
-        dp[n] = solveUsingMem(n-1, dp) + solveUsingMem(n-2, dp);
         return dp[n];
+
+
+
+
 
     }
     int fib(int n) {
