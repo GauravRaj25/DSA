c1c351a95a4d48decf32564287a1d735adaae0ac
Gaurav Raj <gauravraj954647@gmail.com>
2026-08-07 17:19:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (47.30%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-    int solveMem(vector<int>& nums, int i, vector<int>& dp){
-        // Base case
-        if(i >= nums.size()) return 0;
+    // int solveMem(vector<int>& nums, int i, vector<int>& dp){
+    //     // Base case
+    //     if(i >= nums.size()) return 0;
 
-        if(dp[i] != -1){
-            return dp[i];
+    //     if(dp[i] != -1){
+    //         return dp[i];
+    //     }
+
+    //     int include = nums[i] + solveMem(nums, i+2, dp);
+    //     int exclude = 0 + solveMem(nums, i+1, dp);
+    //     dp[i] = max(include, exclude);
+
+    //     return dp[i];
+
+    // }
+
+    int solveTab(vector<int>& nums, vector<int>& dp){
+        // converting base case into dp array 
+        int n = nums.size();
+        dp[n] = 0;
+
+        // run a loop from n to 0
+        for(int i=n-1; i>=0; i--){
+            int include = nums[i] + dp[i+2];
+            int exclude = 0 + dp[i+1];
+            dp[i] = max(include, exclude);
         }
-
-        int include = nums[i] + solveMem(nums, i+2, dp);
-        int exclude = 0 + solveMem(nums, i+1, dp);
-        dp[i] = max(include, exclude);
-
-        return dp[i];
-
+        return dp[0];
     }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1, -1);
-        int i = 0;
-        return solveMem(nums, i, dp);
+        vector<int> dp(n+2, 0);
+        // int i = 0;
+        // return solveMem(nums, i, dp);
+
+        return solveTab(nums, dp);
     }
 };
