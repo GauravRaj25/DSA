dfa1e50c5d8dd86098a71ccc0be68143f079d8ec
Gaurav Raj <gauravraj954647@gmail.com>
2026-08-14 11:03:18 +0530

    Time: 2 ms (15.15%) | Memory: 20.5 MB (97.03%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -2,29 +2,36 @@ class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         // 1. Brute Approach
-        vector<int> even;
-        vector<int> odd;
-        vector<int>ans;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] % 2 == 0){
-                even.push_back(nums[i]);
-            }
-            else{
-                odd.push_back(nums[i]);
-            }
-        }
+        // vector<int> even;
+        // vector<int> odd;
+        // vector<int>ans;
+        // for(int i=0; i<nums.size(); i++){
+        //     if(nums[i] % 2 == 0){
+        //         even.push_back(nums[i]);
+        //     }
+        //     else{
+        //         odd.push_back(nums[i]);
+        //     }
+        // }
 
-        for(int x : even){
-            ans.push_back(x);
-        }
+        // for(int x : even){
+        //     ans.push_back(x);
+        // }
 
-        for(int y : odd){
-            ans.push_back(y);
-        }
+        // for(int y : odd){
+        //     ans.push_back(y);
+        // }
 
-        return ans;
+        // return ans;
 
         // 2. Optimal Approach
-        
+        int j = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] % 2 == 0){
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+        return nums;
     }
 };
